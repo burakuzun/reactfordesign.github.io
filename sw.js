7d2aaef09423e08c75cc129e6ea71051309c8d96
Philip Davis <Phillysoul11@gmail.com>
2019-01-23 08:07:03 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b5942aaf7d070b8f91c5.js"
+    "url": "webpack-runtime-cac6a8b7f9b23ca225ea.js"
   },
   {
     "url": "app-2acf5ce2db0fbbc6469f.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3c6730b4f0e15bd78cad0c3fd32f3010"
+    "revision": "6411da3dde318a27b812c31d80efb9c3"
   },
   {
     "url": "0-033703b67a3f5ba6a410.js"
